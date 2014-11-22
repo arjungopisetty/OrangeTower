36877ef3d9f633b2a59cb92b0a4deafcbd1c5bcf
Arjun Gopisetty <arjun.gopisetty@gmail.com>
2014-11-21 19:37:58 -0600

    Initial commit

---
+++ app/src/main/java/com/arjungopisetty/orangetower/IntroActivity.java
@@ -0,0 +1,107 @@
+package com.arjungopisetty.orangetower;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class IntroActivity extends Activity {
+
+    private Button mNextActivityButton;
+    private String mStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        mNextActivityButton = (Button) findViewById(R.id.next_activity_button);
+        mNextActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String text = getHtml("http://www.utexas.edu/operations/");
+                    mStatus =  getStatus(text);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                Intent intent = new Intent(getApplicationContext(), TowerColorActivity.class);
+                intent.putExtra("status", mStatus);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.intro, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static String getHtml(String url) throws ClientProtocolException, IOException {
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpContext localContext = new BasicHttpContext();
+        HttpGet httpGet = new HttpGet(url);
+        HttpResponse response = httpClient.execute(httpGet, localContext);
+        String result = "";
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        response.getEntity().getContent()
+                )
+        );
+
+        String line = null;
+        while ((line = reader.readLine()) != null){
+            result += line + "\n";
+        }
+        return result;
+    }
+
+    public static String getStatus(String html){
+        Integer pos = html.indexOf("<a href=\"http://www.utexas.edu/spotlight/tower_lighting.html\">");
+        System.out.println("pos of status: " + pos);
+        String test = html.substring(pos+62, pos+70);
+        if (test.equals("standard"))
+            return "The tower is not orange";
+        else
+            return "The Tower is orange";
+
+    }
+}
---
+++ app/src/main/java/com/arjungopisetty/orangetower/TowerColorActivity.java
@@ -0,0 +1,46 @@
+package com.arjungopisetty.orangetower;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class TowerColorActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tower_color);
+
+        Intent i = getIntent();
+        String color = i.getStringExtra("status");
+        mTextView = (TextView) findViewById(R.id.color_textview);
+        mTextView.setText(color);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        getMenuInflater().inflate(R.menu.tower_color, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
